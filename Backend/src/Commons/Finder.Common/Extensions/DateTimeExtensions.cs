ffa565e245eefe1e727c7b967b13d906d45cb502
HILTON <artyomhilton.tech@yahoo.com>
2026-06-04 21:59:20 +0300

    Добавлен editorconfig и методы расширения для DateTime

---
+++ Backend/src/Commons/Finder.Common/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Finder.Common.Extensions;
+
+public static class DateTimeExtensions
+{
+    extension(DateTime dateTime)
+    {
+        public DateOnly ToDateOnly() =>
+            DateOnly.FromDateTime(dateTime);
+
+        public TimeOnly ToTimeOnly() =>
+            TimeOnly.FromDateTime(dateTime);
+    }
+}
