0aaa1e8d8c9097534c8fc71d88eadbeba0b08ce1
HILTON <artyomhilton.tech@yahoo.com>
2026-06-04 22:08:47 +0300

    Добавлены методы расширения

---
+++ Backend/src/Commons/Finder.Common/Extensions/ObjectExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Finder.Common.Extensions;
+
+public static class ObjectExtensions
+{
+    extension(object? @object)
+    {
+        public bool IsNull() =>
+            @object is null;
+
+        public bool IsNotNull() =>
+            @object is not null;
+    }
+}
---
+++ Backend/src/Commons/Finder.Common/Extensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Finder.Common.Extensions;
+
+public static class StringExtensions
+{
+    extension(string @string)
+    {
+        public bool IsNotNullOrEmpty() =>
+            !string.IsNullOrEmpty(@string);
+
+        public bool IsNotNullOrWhiteSpace() =>
+            !string.IsNullOrWhiteSpace(@string);
+    }
+}
