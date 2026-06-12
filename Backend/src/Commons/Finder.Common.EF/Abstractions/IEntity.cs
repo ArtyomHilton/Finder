e5e6456912b28810a59d4988894ddfd806af928e
Artyom <artyomhilton@gmail.com>
2026-06-12 22:46:16 +0300

    FIN-3: Настройка общего проекта для EF (#3)
    Добавлены базовые настройки для проектов с EF CORE
    
    ---------
    
    Co-authored-by: HILTON <artyomhilton.tech@yahoo.com>

---
+++ Backend/src/Commons/Finder.Common.EF/Abstractions/IEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Finder.Common.EF.Abstractions;
+
+public interface IEntity { }
+
+public interface IEntity<T> : IEntity
+{
+    T Id { get; set; }
+}
---
+++ Backend/src/Commons/Finder.Common.EF/Abstractions/IHaveTimeStamp.cs
@@ -0,0 +1,7 @@
+﻿namespace Finder.Common.EF.Abstractions;
+
+public interface IHaveTimestamp
+{
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
