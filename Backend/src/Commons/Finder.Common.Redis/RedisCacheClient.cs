2a7258508d034be632cd182d16980fea29ca6ad3
HILTON <artyomhilton.tech@yahoo.com>
2026-06-21 20:15:56 +0300

    Добавил Cache client

---
+++ Backend/src/Commons/Finder.Common.Redis/RedisCacheClient.cs
@@ -0,0 +1,17 @@
+﻿using Finder.Common.Extensions;
+using StackExchange.Redis;
+
+namespace Finder.Common.Redis;
+
+class RedisCacheClient(IDatabase redis) : ICacheClient
+{
+    public async Task<T?> GetAsync<T>(string key)
+    {
+        var value = await redis.StringGetAsync(key);
+
+        return value.ToString().FromJson<T>();
+    }
+
+    public async Task SetAsync<T>(string key, T value, TimeSpan? ttl) =>
+        await redis.StringSetAsync(key, redis.ToJson(), ttl, false);
+}
---
+++ Backend/src/Commons/Finder.Common.Redis/RedisDependencyInjections.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace Finder.Common.Redis;
+
+public static class RedisDependencyInjections
+{
+    extension(IServiceCollection serviceCollection)
+    {
+        public IServiceCollection AddCacheClient(IConfiguration configuration)
+        {
+            serviceCollection.AddStackExchangeConfiguration(configuration);
+
+            return serviceCollection;
+        }
+
+        private IServiceCollection AddStackExchangeConfiguration(IConfiguration configuration)
+        {
+            serviceCollection.Configure<RedisConfiguration>(configuration.GetSection(nameof(RedisConfiguration)));
+
+            serviceCollection.AddSingleton<IDatabase>(sp =>
+            {
+                var redisConfiguration = sp.GetRequiredService<RedisConfiguration>();
+
+                var multiplexer = ConnectionMultiplexer.Connect(redisConfiguration.ConnectionString);
+
+                return multiplexer.GetDatabase();
+            });
+
+            serviceCollection.AddSingleton<ICacheClient, RedisCacheClient>();
+
+            return serviceCollection;
+        }
+    }
+}
