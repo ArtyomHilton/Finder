a57b0344f01ca053f9af8c2b31030d6194c68a0b
Artyom <artyomhilton@gmail.com>
2026-06-21 21:02:14 +0300

    Добавил Cache client (#4)
    Co-authored-by: HILTON <artyomhilton.tech@yahoo.com>

---
+++ Backend/src/Commons/Finder.Common.Redis/ICacheClient.cs
@@ -0,0 +1,7 @@
+﻿namespace Finder.Common.Redis;
+
+public interface ICacheClient
+{
+    Task SetAsync<T>(string key, T value, TimeSpan? ttl);
+    Task<T?> GetAsync<T>(string key);
+}
---
+++ Backend/src/Commons/Finder.Common.Redis/RedisConfiguration.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Finder.Common.Redis;
+
+public class RedisConfiguration
+{
+    [ConfigurationKeyName("REDIS_CONNECTION_STRING")]
+    public string ConnectionString { get; set; } = null!;
+}
